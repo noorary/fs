2604a405dd20b8f928094bee04956ee28a1aa877
noorarytila <rytilanoora@gmail.com>
2020-12-17 16:53:01 +0200

    tehtävä 4.2 tehty: refaktoroitu koodi moduuleihin

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const router = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.erro('error connection to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', router)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
