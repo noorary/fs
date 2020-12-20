6ceb807a1bbaa471d0dcc01943a62d5b2e480685
noorarytila <rytilanoora@gmail.com>
2020-12-20 17:00:44 +0200

    tehtävät 4.15-4.16 tehty

---
+++ osa4/blogilista/models/users.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        minlength: 3,
+        unique: true
+    },
+    name: {
+        type: String,
+    },
+    passWordHash: String,
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passWordHash
+    }
+})
+
+userSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('User', userSchema)
---
+++ osa4/blogilista/tests/users.test.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const helper = require('./test_helper')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const User = require('../models/users')
+
+describe('when there is initially one user at db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })
+
+    test('creating new user', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'kayttaja',
+            name: 'Perus Kayttaja',
+            password: 'maitolaatikko'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+
+    test('creation fails with proper statuscode and message is username is already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            name: 'Superuser',
+            password: 'salainensana'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('`username` to be unique')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
+
+    test('creating new user fails with proper status code and message if username is shorter than 3 characters', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'ei',
+            name: 'Ei Oo',
+            password: 'salainensana'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+
+        expect(result.body.error).toContain('shorter than the minimum allowed lengt')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+    })
+
+    test('creating new user fails with proper status code and message if password is shorter than 3 characters', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'sammie',
+            name: 'Sam Samson',
+            password: '12'
+        }
+
+        const response = await api.post('/api/users').send(newUser)
+        console.log(response.body)
+        expect(response.body.error).toContain('password must be at least 3 charachters')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+    })
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
