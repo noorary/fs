da18f77a8a65eb1422262b8709cc66c7e383e55e
noorarytila <rytilanoora@gmail.com>
2021-02-24 19:57:16 +0200

    tehtävät 6.13-6.18 tehty

--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -13,7 +13,7 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     dispatch(addAnecdote(content))
-    //dispatch(setNotification(`you added '${content}'`, 5))
+    dispatch(setNotification(`you added '${content}'`, 5))
   }
 
   return(
